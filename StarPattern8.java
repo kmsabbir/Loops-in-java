3074e3cb0e3ebd83e8b7589c1674580e39563062
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-11-04 00:31:22 +0600

    Update StarPattern8.java

--- StarPattern8.java
+++ StarPattern8.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package New.folder;
 
-/**
- *
- * @author WIN
- */
 public class StarPattern8 {
     public static void main(String[] args) {
         for(int i=1;i<=6;i++){
