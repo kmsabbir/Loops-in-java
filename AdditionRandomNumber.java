23c0fdb8b631106a89c509fb4f281e53516c4acd
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-11-05 00:59:00 +0600

    Add files via upload

---
+++ AdditionRandomNumber.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package New.folder;
+
+/**
+ *
+ * @author WIN
+ */
+public class AdditionRandomNumber {
+    public static void main(String[] args) {
+        int count=0;
+        for(int i=1;i<=6;i++){
+            count=count+i;
+            
+        }
+        System.out.println(count);
+    }
+    
+}
---
+++ NumberPattern4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package New.folder;
+
+/**
+ *
+ * @author WIN
+ */
+public class NumberPattern4 {
+     public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                
+                System.out.print(j+" ");
+                }
+            for(int k=i-1;k>=1;k--){
+                
+                System.out.print(k+" ");
+                }
+            System.out.println();
+            }
+           // System.out.println();
+        }
+    
+}
