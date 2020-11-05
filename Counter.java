5af7cfad9a76d8ee8a1dcfd44436a083ccca35b7
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-11-06 01:18:53 +0600

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package New.folder;
+
+/**
+ *
+ * @author WIN
+ */
+public class Counter {
+     public static void main(String[] args) {
+       int counter =0;
+         for(int i=1;i<=6;i++){
+            for(int j=1;j<=i;j++){
+                counter=counter+1;
+                System.out.print(counter+" ");
+                }
+            System.out.println();
+            }
+           // System.out.println();
+     }
+}
