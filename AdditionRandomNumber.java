442ff0533b35b60f9f8d5afcc35a96f3436aa1b0
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-11-15 23:42:26 +0600

    Update AdditionRandomNumber.java

--- AdditionRandomNumber.java
+++ AdditionRandomNumber.java
@@ -5,6 +5,7 @@ public class AdditionRandomNumber {
     public static void main(String[] args) {
         int count=0;
         for(int i=1;i<=6;i++){
+
             count=count+i;
             
         }
