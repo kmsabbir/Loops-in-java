d72a71b3597c342f653854181e17eb108eba1fbd
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2020-12-14 23:52:19 +0600

    Add files via upload

---
+++ PrintFirstDuplicateElement.java
@@ -0,0 +1,22 @@
+package com.forloop;
+
+public class PrintFirstDuplicateElement {
+    
+    public static void main(String[] args) {
+    int [] a={6,5,3,2,5,2,4,1};
+    for(int i =0;i<a.length-1;i++){
+            int temp =0;
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]==a[j]){
+                    System.out.println("First duplicate number is: "+a[i]);
+                    temp=temp+1;
+                    break;
+                }
+            
+            }
+            if(temp>0){
+            break;
+            }
+        }
+    }
+}
