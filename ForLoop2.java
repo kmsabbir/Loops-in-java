9c5b0977d9fef79adbf770dbdbf021b86f29c5b6
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2021-01-07 00:06:55 +0600

    Update ForLoop2.java

--- ForLoop2.java
+++ ForLoop2.java
@@ -1,5 +1,5 @@
 public class ForLoop2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         for(int i=1;i<=4;i++){
          
            for(int j=1;j<=i;j++){
